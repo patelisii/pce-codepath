f786c4aeb3f8e1e3b014e10d5ab7a2334fa6388b
patelisii <34968106+patelisii@users.noreply.github.com>
2020-11-07 16:22:26 -0500

    Initial Commit

---
+++ calc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calc
+//
+//  Created by Patrick Elisii on 11/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
